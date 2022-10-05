d374f428c2fff497d1f0f581d624c7640fec6c62
dlopez1591 <83315962+dlopez1591@users.noreply.github.com>
2022-10-05 00:46:44 -0500

    Add files via upload

---
+++ ejerciciostema1/src/com/openbootcamp/EjerciciosTema1.java
@@ -0,0 +1,30 @@
+package com.openbootcamp;
+
+import java.sql.SQLOutput;
+
+public class EjerciciosTema1 {
+    public static void main(String[] args) {
+        //numericos
+        int entero = 30;
+        System.out.println("Soy el entero: " + entero);
+        byte bite = 1;
+        System.out.println("Soy el Byte: " + bite);
+        long numerotelefono = 123456;
+        System.out.println("Soy un long: " + numerotelefono);
+        short pequeno = 1;
+        System.out.println("Soy un short: " + pequeno);
+        double decimal = 1.25d;
+        System.out.println("Soy un double: " + decimal);
+        float decimaldos = 1.1f;
+        System.out.println("Soy un foat: " + decimaldos);
+        //boolean
+        boolean estudio = true;
+        System.out.println("Soy un booleano: " + estudio);
+        //Texto
+        String cadena = "Soy un String";
+        System.out.println(cadena);
+        char caracter = '2';
+        System.out.println("Soy un char: " + caracter);
+
+    }
+}
